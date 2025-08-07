1662fa08bd5be25f16ee931a59db114ca0a6b081
DJ Sweder <djsweder@gmail.com>
2025-08-07 10:17:18 +0200

    Text of title changed

--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zobrazení zprávy</title>
+    <title>Odesílatel tohoto odkazu Vám posílá soukromou zprávu</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
